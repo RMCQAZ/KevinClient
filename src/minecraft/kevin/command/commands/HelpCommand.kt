b34cbf267f1ed0a4a8be7b649f554604f12a772f
杰哥教你登dua郎 <2998257409@qq.com>
2021-08-16 17:10:42 +0800

    Update
    Update help command
    Add jars required for running

--- src/minecraft/kevin/command/commands/HelpCommand.kt
+++ src/minecraft/kevin/command/commands/HelpCommand.kt
@@ -8,5 +8,12 @@ class HelpCommand : ICommand {
         ChatUtils().message("§b<Help>")
         ChatUtils().message("§a.t/.toggle <ModuleName> <on/off> §9Enable/Disable module.")
         ChatUtils().message("§a.h/.help §9Show this message.")
+        ChatUtils().message("§a.binds §9Show binds.")
+        ChatUtils().message("§a.bind <Module> <Key> §9Bind Module To a Key.")
+        ChatUtils().message("§a.binds clear §9Clear binds.")
+        ChatUtils().message("§a.login <Name> <Password?> §9Login.")
+        ChatUtils().message("§a.say §9Say.")
+        ChatUtils().message("§a.modulestate §9Show module state.")
+        ChatUtils().message("§a.<ModuleName> <Option> <Value> §9Set module option value.")
     }
 }
