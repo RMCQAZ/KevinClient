cdf432ce98c0645d7218edf5f7d6ab386435ac0c
杰哥教你登dua郎 <2998257409@qq.com>
2021-09-02 22:05:15 +0800

    Fix AAC5Fly

--- src/minecraft/kevin/module/modules/movement/Fly.kt
+++ src/minecraft/kevin/module/modules/movement/Fly.kt
@@ -1,6 +1,8 @@
 package kevin.module.modules.movement
 
 import kevin.event.*
+import kevin.hud.element.elements.Notification
+import kevin.main.Kevin
 import kevin.module.*
 import kevin.utils.MSTimer
 import kevin.utils.MovementUtils
@@ -52,6 +54,13 @@ class Fly : Module("Fly","Allow you fly", Keyboard.KEY_F,ModuleCategory.MOVEMENT
         when(mode.get()){
             "Creative" -> mc.thePlayer.capabilities.allowFlying = true
             "AAC5" -> {
+                if (mc.isSingleplayer) {
+                    Kevin.getInstance.hud.addNotification(
+                        Notification("Use AAC5.2.0 Flys will crash single player"),"Fly")
+                    toggle(false)
+                    return
+                }
+
                 flyTimer.reset()
                 aac5FlyClip=false
                 aac5FlyStart=false
@@ -73,6 +82,7 @@ class Fly : Module("Fly","Allow you fly", Keyboard.KEY_F,ModuleCategory.MOVEMENT
                 }
             }
             "AAC5"->{
+                if (mc.isSingleplayer) return
                 sendAAC5Packets()
                 mc.thePlayer.noClip = false
                 mc.timer.timerSpeed = 1F
@@ -108,15 +118,22 @@ class Fly : Module("Fly","Allow you fly", Keyboard.KEY_F,ModuleCategory.MOVEMENT
                     mc.thePlayer.motionZ = 0.0
                     mc.thePlayer.jumpMovementFactor = 0.00f
                     mc.timer.timerSpeed = 0.32F
-                    return;
+                    return
                 }else {
                     if(!aac5FlyClip) {
                         mc.timer.timerSpeed = 0.19F
                     }else{
-                        aac5FlyClip=false;
+                        aac5FlyClip=false
                         mc.timer.timerSpeed = 1.2F
                     }
                 }
+                mc.thePlayer.motionY = 0.0
+                mc.thePlayer.motionX = 0.0
+                mc.thePlayer.motionZ = 0.0
+                mc.thePlayer.capabilities.isFlying = false
+                if (mc.gameSettings.keyBindJump.isKeyDown) mc.thePlayer.motionY += speed.get()
+                if (mc.gameSettings.keyBindSneak.isKeyDown) mc.thePlayer.motionY -= speed.get()
+                MovementUtils.strafe(speed.get())
             }
         }
     }
@@ -143,6 +160,7 @@ class Fly : Module("Fly","Allow you fly", Keyboard.KEY_F,ModuleCategory.MOVEMENT
             //if (packet.x == 1.7E+301||packet.y == Double.MAX_VALUE||packet.z == 1.7E+301) return
             when(mode.get()){
                 "AAC5" -> {
+                    event.cancelEvent()
                     val f = mc.thePlayer.width / 2.0
                     // need to no collide else will flag
                     if(aac5nextFlag || !mc.theWorld.checkBlockCollision(AxisAlignedBB(packet.x - f, packet.y, packet.z - f, packet.x + f, packet.y + mc.thePlayer.height, packet.z + f))){
